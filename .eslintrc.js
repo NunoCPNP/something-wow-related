4964e13d4de883488ba0d0bd65a201d71733da26
Nuno Nunes Pereira <nuno.nunespereira@ffptn3436.home>
2020-11-20 23:28:17 +0000

    feat(monorepo): Initial boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
